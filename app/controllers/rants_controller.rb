1791b57ef86f9b9dada8693bdbd76e3235bb9c1f
dcuddhy <dcuddhy@gmail.com>
2015-03-14 22:20:09 -0600

    user can only delete and edit their own rants

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -27,17 +27,22 @@ class RantsController < ApplicationController
 
   def update
     @rant = Rant.find(params[:id])
-    if @rant.update(params.require(:rant).permit(:title, :body))
+    if @rant.user_id == current_user.id
+      @rant.update(params.require(:rant).permit(:title, :body))
       redirect_to rants_path, notice: 'Rant was successfully edited.'
     else
-      render :edit
+      render :edit, notice: "Cannot edit this rant"
     end
   end
 
   def destroy
     @rant = Rant.find(params[:id])
-    @rant.destroy
-    redirect_to root_path, notice: 'Rant was successfully destroyed.'
+    if @rant.user_id == current_user.id
+      @rant.destroy
+      redirect_to root_path, notice: "Rant was successfully destroyed."
+    else
+      render :edit, notice: "Cannot delete this rant"
+    end
   end
 
 
