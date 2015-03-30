3bb9232a3e67c6d3da592a476d7928d02de083a9
dcuddhy <dcuddhy@gmail.com>
2015-03-30 10:27:01 -0600

    implimented gravatar on rant index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "rants#index"
   get '/signup' => 'registrations#new', as: :signup
   post '/signup' => 'registrations#create'
-  get '/signin' => 'authentication#new', as: :signin
+    get '/signin' => 'authentication#new', as: :signin
   post '/signin' => 'authentication#create'
   get '/signout' => 'authentication#destroy', as: :signout
   get '/search' => 'rants#index', as: :search
